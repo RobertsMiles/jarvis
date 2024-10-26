b38b2b2165dd29494fef3c1f559be8cfc0f393c6
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-26 17:24:02 -0500

    added text-to-speech

--- main.sh
+++ main.sh
@@ -1,18 +1,25 @@
 # pass in todo.txt, current time, thunderbird integration, etc
+# replace todo.txt with recursively scanned obsidian directory
 
-# user input file
-printf "You:\n"
+# user input to file
+printf ">>> "
 read -r input
-echo "$input" > argument.txt
+printf "$input" > temp/argument.txt
 
 # generate date file
-printf "The current date and time is: " > date.txt
-date +"%I:%M %p, %A %B %d, %Y" >> date.txt
+printf "The current date and time is: " > temp/date.txt
+date +"%I:%M %p, %A %B %d, %Y" >> temp/date.txt
 
-# pass files to llm
-printf "\nAssistant:\n"
-cat instruction.txt date.txt todo.txt argument.txt | ollama run llama3.2
+# pass files to ollama; output to file; save ollama session
+cat files/instruction.txt temp/date.txt files/todo.txt temp/argument.txt | ollama run llama3.2 --keepalive 1m > temp/output.txt
+
+# print output
+cat temp/output.txt
+
+# text-to-speech
+cat temp/output.txt | flite --setf duration_stretch=0.7
 
 # remove temporary files
-rm argument.txt
-rm date.txt
+rm temp/argument.txt
+rm temp/date.txt
+rm temp/output.txt
